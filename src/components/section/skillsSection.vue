67e2423b9aed3a797d3bf826da72cac586be2726
n4oah <n4oahdev@gmail.com>
2020-07-19 21:52:03 +0900

    스킬 뱃지 형식으로 변경

--- src/components/section/skillsSection.vue
+++ src/components/section/skillsSection.vue
@@ -1,16 +1,19 @@
 <template>
 <div v-if="$isDataEmpty(data) == false">
     <div class="rela-block caps greyed">Skills</div>
-    <div v-for="item in data">
+    <div v-for="(item, index) in data" :key="index">
         <ul>
             <li><p class="font-weight-bolder text-medium">{{ item.name }}</p>
-                <ul>
-                    <div v-for="skill in item.skill">
-                        <li class="medium-margin">
-                            <span>{{ skill.name }}</span>
-                            <b-progress variant="success" :max="10" :value="parseInt(skill.level)" :show-value="true"></b-progress>
+                <ul style="list-style:none;">
+                    <template v-for="(skill, idx) in item.skill" >
+                        <!-- <div class="text-center"> -->
+                        <li :key="idx" style="display: inline-block;">
+                            <b-badge variant="primary" style="margin-right: 1vh; margin-bottom: 1vh; font-size: 1.7vh; padding: 1vh;">{{ skill.name }}</b-badge>
                         </li>
-                    </div>
+                        <!-- </div> -->
+                        <!-- <span>{{ skill.name }}</span> -->
+                        <!-- <b-progress variant="success" :max="10" :value="parseInt(skill.level)" :show-value="true"></b-progress> -->
+                    </template>
                 </ul>
             </li>
         </ul>
